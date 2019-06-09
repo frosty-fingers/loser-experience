b41e9af3bd75edfd9a48541e9af7317080436ec4
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-09 21:35:26 +1000

    craeted license-list and created mockup license data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LicenseListComponent } from './licenses/license-list/license-list.component';
+
+
+const routes: Routes = [
+  { path: 'licenses', component: LicenseListComponent },
+  { path: '', redirectTo: '/licenses', pathMatch: 'full'},
+  { path: '**', redirectTo: '/licenses', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

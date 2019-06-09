c376b6ee811c0ed60d892f9621aecbbe52601b22
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-09 22:50:47 +1000

    got rid of bootstrap

--- src/app/licenses/license-list/license-list.component.ts
+++ src/app/licenses/license-list/license-list.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-license-list',
   templateUrl: './license-list.component.html',
   styleUrls: ['./license-list.component.scss']
 })
 export class LicenseListComponent implements OnInit {
-
-  constructor() { }
+  pageTitle: string = 'Licenses';
+  licenses: any[] = [
+    {
+        "createdBy": "James Volis",
+        "createdOnUtc": "2019-04-23T18:25:43.511Z",
+        "customerCode": "JVOL",
+        "expiresAfterDays": 365,
+        "expiresOnUtc": "2020-04-23T18:25:43.511Z",
+        "id": 1,
+        "licenseeName": "J.P.Volis",
+        "licenseeOrganisation": "Jamesocentric",
+        "licenseIdentifier": "54321",
+        "productCode": "12345",
+        "productName": "James's app",
+        "productReleaseVersion": "8.1.0"
+    },
+    {
+        "createdBy": "James Volis",
+        "createdOnUtc": "2018-04-23T18:25:43.511Z",
+        "customerCode": "JVOL",
+        "expiresAfterDays": 365,
+        "expiresOnUtc": "2019-04-23T18:25:43.511Z",
+        "id": 2,
+        "licenseeName": "J.P.Volis",
+        "licenseeOrganisation": "Jamesoferic",
+        "licenseIdentifier": "69789",
+        "productCode": "98798",
+        "productName": "James's other app",
+        "productReleaseVersion": "4.1.0"
+    },
+    {
+        "createdBy": "James Volis",
+        "createdOnUtc": "2019-04-23T18:25:43.511Z",
+        "customerCode": "JVOL",
+        "expiresAfterDays": 365,
+        "expiresOnUtc": "2020-04-23T18:25:43.511Z",
+        "id": 3,
+        "licenseeName": "J.P.Volis",
+        "licenseeOrganisation": "Jamesocentric",
+        "licenseIdentifier": "54321",
+        "productCode": "12345",
+        "productName": "James's app",
+        "productReleaseVersion": "8.1.0"
+    }
+  ];
+  
+  constructor() {}
 
   ngOnInit() {
+
   }
 
 }
---
+++ src/app/licenses/license.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LicenseService {
+  private licenseUrl = 'api/licenses/license.json';
+
+  constructor(private http: HttpClient) {}
+
+  getLicenses(): Observable<any[]>{
+    return this.http.get<any[]>(this.licenseUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+        errorMessage = `An error occured: ${err.error.message}`;
+    } else {
+        errorMessage = `Server returned code ${err.status}, error message is: ${err.message}`
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}

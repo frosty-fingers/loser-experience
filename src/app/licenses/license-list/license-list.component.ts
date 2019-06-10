1ca409fa8e43b28757e894e71ec186c3f5e72589
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-11 08:33:07 +1000

    Adding to github

--- src/app/licenses/license-list/license-list.component.ts
+++ src/app/licenses/license-list/license-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { License } from '../license';
+import { SortService, ColumnSortedEvent } from 'src/app/shared/sort/sort.service';
 
 
 @Component({
@@ -53,11 +54,26 @@ export class LicenseListComponent implements OnInit {
         "productReleaseVersion": "8.1.0"
     }
   ];
-  slicedLicenses: License[];  
+  sortedLicenses: License[];  
 
-  performSort
+  _listSorter: ColumnSortedEvent;
+  get listSorter(): ColumnSortedEvent{
+    return this._listSorter;
+  }
+  set listSorter(value: ColumnSortedEvent) {
+    this._listSorter = value;
+    this.sortedLicenses = this.listSorter ? this.performSort(this.listSorter) : this.licenses;
+  }
 
-  constructor() {}
+  performSort(columnSortedEvent: ColumnSortedEvent): License[] {
+    let sortColumn = columnSortedEvent.sortColumn;
+    let sortDirection = columnSortedEvent.sortDirection;
+    if sortColumn instanceof string
+      return this.licenses.sort((a: License, b: License) => 
+      JSON.stringify(a[sortColumn]).localeCompare(JSON.stringify(b[sortColumn]));
+  }
+
+  constructor(private sortService: SortService) {}
 
   ngOnInit() {
 
